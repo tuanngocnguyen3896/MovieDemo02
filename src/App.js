2d6ac765642f32d6b1e4056aa31d9487209fd7ee
tuanngocnguyen3896 <76700670+tuanngocnguyen3896@users.noreply.github.com>
2021-04-18 23:20:56 +0700

    'simple_movie_web'

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <SimpleBottomNavigation />
+      {/* <Header /> */}
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
